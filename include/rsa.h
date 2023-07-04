f27d12a96af0e12cd49c3ebd291f78f8061addf7
Luiz Schonarth <luizcschonarth@gmail.com>
2023-07-03 22:29:25 -0300

    WIP: Started is_probably_prime implementation

--- include/rsa.h
+++ include/rsa.h
@@ -1,6 +1,8 @@
 #ifndef __RSA_H
 #define __RSA_H
 
+#include <gmp.h>
+
 void rsa_gen_pq();
 void rsa_sign();
 void rsa_verify();
--- rsa.c
+++ rsa.c
@@ -1,1 +1,31 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include "rsa.h"
+
+static bool is_probably_prime(mpz_t prime, size_t n_iterate);
+
+static bool is_probably_prime(mpz_t num, size_t n_iterate) {
+    // The number generated is always odd and bigger then 2^(n_bits-1)-1 since
+    // we've set the 0th and (k-1)th bits as 1, so there is no need to check if
+    // the number is even.
+
+    // Represents n - 1 = 2^s*d
+
+#ifdef TODO
+    for (size_t i = 0; i < n_iterate; i++) {
+        //Inicializa as coisas prox a aleatorio
+        if (mpz_cmp_ui(a, 1) == 0 || mpz_cmp_ui(a, n_minus_one) == 0) {
+            continue;
+        }
+        for (size_t r = 1; r < s; r++) {
+            // y = x * x
+            if (mpz_cmp_ui(y, n_minus_one) == 0) {
+                goto cnt;
+            }
+        }
+        return false;
+        cnt:
+    }
+    return true;
+#endif
+}
